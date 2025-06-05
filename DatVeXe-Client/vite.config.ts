4083ee48dca7ac8807518c00bcccfd8889ac7b2a
longhuynhdev <longhuynhdev@gmail.com>
2025-06-05 11:28:31 +0700

    feat: integrate TanStack Router

--- DatVeXe-Client/vite.config.ts
+++ DatVeXe-Client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    // Doc: https://tanstack.com/router/latest/docs/framework/react/quick-start
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+    //Doc: https://tailwindcss.com/docs/installation/using-vite
+    tailwindcss(),
+  ],
+});
